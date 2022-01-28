6156780276b2ef35575a5b707860f019e30d0aa1
apitongcm <apitongcm@gmail.com>
2022-01-27 20:27:22 -0800

    added the background loop with gravity

---
+++ Bird.lua
@@ -0,0 +1,36 @@
+--third commit 
+-- adding the class of Bird
+Bird = Class{}
+
+--fouth commit 
+--adding gravity to the game 
+local GRAVITY = 20
+
+function Bird:init()
+    --third commit 
+    -- adding the image of the bird on the game 
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    --third commit 
+    --adding the starting position of the bird
+    self.x = VIRTUAL_WIDTH / 3 - (self.width /2 )
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2 )  
+
+    --fourth commit 
+    --adding starting velocity of the bird
+    self.dy = 0 
+end
+
+--fourth commit 
+--applying gravity to velocity
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt 
+
+    self.y = self.y + self.dy
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
---
+++ main.lua
@@ -0,0 +1,89 @@
+push = require 'push'
+
+--third commit 
+--adding the class module in the main code
+Class = require 'class'
+--adding the bird class 
+require 'Bird'
+
+--constant variables 
+WINDOW_WIDTH  = 1280 
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288 
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0 
+
+--second commit adding scrolling speed for the background
+--speed at which we should scroll our image
+local BACKGROUND_SCROLL_SPEED = 30 
+local GROUND_SCROLL_SPEED = 60
+local BACKGROUND_LOOPING_POINT = 413
+
+--third commit define bird sprite
+local bird = Bird()
+
+function love.load()
+    --init nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    --app window title 
+    love.window.setTitle('Fluffy Bird')
+
+    --init virtual resolution 
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true, 
+        fullscreen = false, 
+        resizable = true
+    })
+end 
+
+function love.resize(w,h)
+    push:resize(w,h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then 
+        love.event.quit()
+    end
+end
+
+-- second commit 
+--adding love.update for the background
+function love.update(dt)
+    --scroll background by preset speed * dt 
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    --fourth commit 
+    --adding velocity/gravity to the bird 
+    bird:update(dt)
+ 
+end
+
+
+function love.draw()
+    push:start()
+    --for the second commit 
+    --we changes the x position of the background and ground into
+    -- -backgroundScroll and -groundScroll
+
+
+    --draw the background starting from top left
+    love.graphics.draw(background,-backgroundScroll,0)
+    --draw the ground on top of the background
+    love.graphics.draw(ground,-groundScroll, VIRTUAL_HEIGHT - 16)
+    
+    --third commit 
+    --render the bird image 
+    bird:render()
+
+
+    push:finish()
+end
